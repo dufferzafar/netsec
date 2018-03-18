32c9197c1f1e3a5494b67dff2bad5da61db91872
Shadab Zafar <dufferzafar0@gmail.com>
2018-03-18 22:11:28 +0530

    TSA: ClientRequester updates

--- 2 - TSA/client_requester.py
+++ 2 - TSA/client_requester.py
@@ -1,7 +1,9 @@
-import socket
-import sys
 import hashlib
 import os
+import socket
+import sys
+
+import rsa
 
 
 class ClientRequester(object):
@@ -11,12 +13,9 @@ class ClientRequester(object):
         # TODO: Hardcode the public key of the tsa
         # self.tsa_pub_key =
 
-        # First twin prime pair after 1 billion
-        # These are the P, Q fed into RSA
-        # self.primes = (1000000093, 1000000097)
-
-        # self.pub_key =
-        # self.pvt_key =
+        # A key-pair generated using: rsa.generate_key_pair(1000003, 1000033)
+        self.pub_key = (172946823661, 1000036000099)
+        self.pvt_key = (640311959845, 1000036000099)
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -30,7 +29,6 @@ class ClientRequester(object):
         # TODO: Proper CLI?
         input_file = sys.argv[1]
 
-        # TODO: Proper os.path.split*
         name, ext = os.path.splitext(input_file)
         output_file = name + "_stamped" + ext
 
@@ -38,25 +36,24 @@ class ClientRequester(object):
         print("> Given input file:", input_file)
 
         with open(input_file, "rb") as inp:
-            dhash = hashlib.sha256(inp.read()).hexdigest()
+            input_data = inp.read()
+            dhash = hashlib.sha256(input_data).hexdigest()
 
         print("> Sending document hash to server:", dhash, "\n")
 
         resp = "HASH: " + dhash
         self.sock.send(resp.encode())
 
-        # TODO: try/except in case something goes wrong?
-        data = self.sock.recv(1024)
+        data = self.sock.recv(4096)
         data = data.decode()
-        print("> Received from server:", data, "\n")
+        # print("> Received from server:", data, "\n")
 
         data = data.split("||")
         now, sig = data[0], data[1]
 
         print("> Dumping data to:", output_file)
         with open(output_file, "w") as out:
-            # out.write(rsa.encrypt(input_file, self.pvt_key))
-            out.write("INPUT")
+            out.write(rsa.encrypt(input_data.decode("ascii"), self.pvt_key))
             out.write("\n")
             out.write(now)
             out.write("\n")
