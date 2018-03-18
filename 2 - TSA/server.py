05bca1606318757da6276254d5d10e6c086453d5
Shadab Zafar <dufferzafar0@gmail.com>
2018-03-18 18:48:13 +0530

    TSA: Server updates

--- 2 - TSA/server.py
+++ 2 - TSA/server.py
@@ -1,11 +1,19 @@
 import socket
 import threading
+import datetime
 
 
 class TimestampServer(object):
-    def __init__(self, host, port):
+    def __init__(self, host='', port=7171):
         self.host = host
         self.port = port
+
+        # First twin prime pair after 1 billion
+        # These are the P, Q fed into RSA
+        self.primes = (1000000007, 1000000009)
+
+        # self.pub_key =
+        # self.pvt_key =
 
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -27,29 +35,41 @@ class TimestampServer(object):
             ).start()
 
     def serve_client(self, client, address):
-        print("> New client connected:", address)
+        print(">>> New client connected:", address)
 
         while True:
             try:
                 data = client.recv(1024)
                 if data:
 
-                    # TODO: Handle all OP_CODES here
-                    response = "Response: " + data.decode() + "\n"
-                    client.send(response.encode())
+                    data = data.decode()
+                    if data.startswith("HASH: "):
+                        dhash = data.lstrip("HASH: ")
+                        print("> Received a document's hash: %s" % dhash)
 
+                        now = str(datetime.datetime.utcnow())
+
+                        # hashlib 256 here
+                        # nhash = dhash + "||" + now
+                        # sig = rsa.sign(nhash, self.pvt_key)
+
+                        sig = "SIG"
+
+                        response = now + "||" + sig
+                        print("> Sending to client:", response)
+                    else:
+                        response = "echo: " + data.decode()
+
+                    client.send(response.encode())
                 else:
                     raise socket.timeout()
 
             except socket.timeout:
-                    print("Client disconnected: ", address, "\n")
+                    print(">>> Client disconnected: ", address, "\n")
                     client.close()
                     return False
 
 
 if __name__ == "__main__":
-
-    # TODO: Get port using CLI
-    port = 7171
-
-    TimestampServer('', port).run()
+    # TODO: Get port from CLI
+    TimestampServer().run()
